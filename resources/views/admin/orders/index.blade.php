cc2b85d63f00cbaab816020168d0536fd3bc5cf0
patriciabrsk <patriciabresku@gmail.com>
2022-06-16 09:40:04 +0200

    add order layout

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -11,18 +11,69 @@
                 @endif
             </div>
 
-        <section class="vh-100">
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
+            {{-- Esempio card ordine --}}
+
+            <div class="col">
+                <div class="card card-stepper">
+                    <div class="card-body p-4">
+
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex flex-column">
+                                <span class="lead fw-normal">L'ordine è stato consegnato</span>
+                                <span class="text-muted small">da *** il 14-06-2022</span>
+                                <p>Nome cliente: ***</p>
+                                <p>Indirizzo: ***</p>
+                                <p>Telefono: ***</p>
+                                <p>Totale: *** €</p>
+                            </div>
+                        <div>
+                        <button class="btn btn-outline-primary" type="button">Traccia la consegna</button>
+                    </div>
+                </div>
+                <hr class="my-4">
+
+                <div class="d-flex flex-row justify-content-between align-items-center align-content-center">
+                    <span class="dot"></span>
+                    <hr class="flex-fill track-line"><span class="dot"></span>
+                    <hr class="flex-fill track-line"><span class="dot"></span>
+                    <hr class="flex-fill track-line"><span class="dot"></span>
+                    <hr class="flex-fill track-line">
+                    <span class="d-flex justify-content-center align-items-center big-dot dot">
+                        <i class="fa fa-check text-white"></i>
+                    </span>
+                </div>
+
+                <div class="d-flex flex-row justify-content-between align-items-center">
+                    <div class="d-flex flex-column align-items-start">
+                        <span>14 Giu</span>
+                        <span>Ordine ricevuto</span>
+                </div>
+                <div class="d-flex flex-column justify-content-center">
+                    <span>14 Giu</span><span>Ordine in elaborazione</span></div>
+                <div class="d-flex flex-column justify-content-center align-items-center"><span>14 Giu</span>
+                    <span>In preparazione</span></div>
+                <div class="d-flex flex-column align-items-center">
+                    <span>14 Giu</span><span>In consegna</span>
+                </div>
+                <div class="d-flex flex-column align-items-end">
+                    <span>14 Giu</span><span>Consegnato</span>
+                </div>
+            </div>
+
+
+                @forelse ($orders as $order)
                     <div class="col">
-                        <div class="card card-stepper" >
+                        <div class="card card-stepper">
                             <div class="card-body p-4">
 
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="d-flex flex-column">
                                         <span class="lead fw-normal">L'ordine è stato consegnato</span>
-                                        <span class="text-muted small">da *** 14-06-2022</span>
-                                        <p>Nome cliente:</p>
+                                        <span class="text-muted small">da *** il {{$order->data}}</span>
+                                        <p>Nome cliente: {{$order->name_customer}}</p>
+                                        <p>Indirizzo: {{$order->address_customer}}</p>
+                                        <p>Telefono: {{$order->phone_number_customer}}</p>
+                                        <p>Totale: {{$order->total_price}} €</p>
                                     </div>
                                 <div>
                                 <button class="btn btn-outline-primary" type="button">Traccia la consegna</button>
@@ -43,26 +94,31 @@
 
                         <div class="d-flex flex-row justify-content-between align-items-center">
                             <div class="d-flex flex-column align-items-start">
-                                <span>14 Giu</span>
+                                <span>{{$order->data}}</span>
                                 <span>Ordine ricevuto</span>
                         </div>
                         <div class="d-flex flex-column justify-content-center">
-                            <span>14 Giu</span><span>Order
-                            placed</span></div>
-                        <div class="d-flex flex-column justify-content-center align-items-center"><span>14 Giu</span>
+                            <span>{{$order->data}}</span><span>Ordine in elaborazione</span></div>
+                        <div class="d-flex flex-column justify-content-center align-items-center"><span>{{$order->data}}</span>
                             <span>In preparazione</span></div>
-                        <div class="d-flex flex-column align-items-center"><span>14 Giu</span><span>In consegna</span></div>
-                        <div class="d-flex flex-column align-items-end"><span>14 Giu</span><span>Consegnato</span></div>
+                        <div class="d-flex flex-column align-items-center">
+                            <span>{{$order->data}}</span><span>In consegna</span>
                         </div>
-
+                        <div class="d-flex flex-column align-items-end">
+                            <span>{{$order->data}}</span><span>Consegnato</span>
                         </div>
+                    @empty
+                        <div class="no-order">
+                            Nessun ordine presente
                         </div>
-                </div>
+                    </div>
+                    @endforelse
                 </div>
             </div>
-            </section>
+        </div>
+    </div>
 
-            <table class="table table-dark table-hover col-12">
+            {{-- <table class="table table-dark table-hover col-12">
                 <thead>
                     <tr>
                         <th class="fw-bold text-primary fs-2">Nome cliente</th>
@@ -97,7 +153,7 @@
                         </tr>
                     @endforelse
                 </tbody>
-            </table>
+            </table> --}}
         </div>
     </div>
 @endsection
