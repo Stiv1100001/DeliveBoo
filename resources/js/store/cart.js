3ecaa8d18eb88f090388cc6eaed227d0c0f07c38
Stefano Bichicchi <bichicchi.stefano@gmail.com>
2022-06-16 10:51:18 +0200

    update cart

--- resources/js/store/cart.js
+++ resources/js/store/cart.js
@@ -12,7 +12,7 @@ export const useStore = defineStore("cart", {
         totalPrice(state) {
             let total = 0;
             state.products.forEach((p) => {
-                total += p.price;
+                total += p.product.price * p.quantity;
             });
 
             return total;
@@ -24,10 +24,36 @@ export const useStore = defineStore("cart", {
     },
 
     actions: {
-        addPrdocutsToCart(product) {
-            this.products.push(product);
+        addProductsToCart(product, quantity = 1) {
+            let toPush = true;
+
+            this.products.forEach((p) => {
+                if (p.product.id == product.id) {
+                    p.quantity += quantity;
+                    toPush = false;
+                }
+            });
+
+            if (toPush) this.products.push({ product, quantity });
 
             // Salvo nel local storage
+            localStorage.setItem("cart", this.products);
+        },
+
+        removeProductsToCart(product, quantity = 1) {
+            const new_products = [];
+
+            this.products.forEach((p) => {
+                if (p.product.id == product.id) {
+                    p.quantity -= quantity;
+                    if (p.quantity > 0) new_products.push(p);
+                } else {
+                    new_products.push(p);
+                }
+            });
+
+            this.products = new_products;
+
             localStorage.setItem("cart", this.products);
         },
     },
