2f23a2ff5bfc4278884544e01dd8755b122f74dd
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-06-16 12:04:20 +0200

    Change to the text of the crud, from English to Italian

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12">
-                <h1 class="text-center fw-bold text-dark mb-5">Modify your dish</h1>
+                <h1 class="text-center fw-bold text-dark mb-5">Modifica il tuo piatto</h1>
             </div>
             <div class="col-6">
                 <form class="border border-3 border-primary rounded p-3 bg-info" action="{{route('admin.dishes.update', $dish)}}" method="POST">
@@ -13,7 +13,7 @@
 
 
                     <div class="mb-3">
-                        <label for="name" class="form-label">Edit the name</label>
+                        <label for="name" class="form-label">Modifica il nome del piatto</label>
                         <input type="text" name="name" id="name" class="form-control" value="{{$dish->name}}">
                         @error('name')
                             <div class="alert alert-danger">
@@ -24,7 +24,7 @@
                 
                     
                     <div class="mb-3">
-                        <label for="description" class="form-label">Edit the description</label>
+                        <label for="description" class="form-label">Modifica la descrizione del piatto</label>
                         <input type="text" name="description" id="description" class="form-control" value="{{$dish->description}}">
                         @error('description')
                             <div class="alert alert-danger">
@@ -35,7 +35,7 @@
                 
                     
                     <div class="mb-3">
-                        <label for="ingredients" class="form-label">Edit the ingredients</label>
+                        <label for="ingredients" class="form-label">Modifica gli ingredienti del piatto</label>
                         <input type="text" name="ingredients" id="ingredients" class="form-control" value="{{$dish->ingredients}}">
                         @error('ingredients')
                             <div class="alert alert-danger">
@@ -46,7 +46,7 @@
                 
                     
                     <div class="mb-3">
-                        <label for="price" class="form-label">Edit the price</label>
+                        <label for="price" class="form-label">Modifica Il prezzo del piatto</label>
                         <input type="text" name="price" id="price" class="form-control" value="{{$dish->price}}">
                         @error('price')
                             <div class="alert alert-danger">
@@ -58,17 +58,17 @@
                     <div class="form-check form-switch">
                         <input class="form-check-input" type="radio" name="availability" id="availability" value="1" {{$dish->availability == 1 ? 'checked' : '' }}>
                         <label class="form-check-label" for="exampleRadios1">
-                            Visibile 
+                            Piatto disponibile 
                         </label>
                     </div>
                     <div class="form-check form-switch">
                         <input class="form-check-input" type="radio" name="availability" id="availability" value="0" {{$dish->availability == 0 ? 'checked' : '' }}>
                         <label class="form-check-label" for="availability">
-                            Non visibile
+                            Piatto non disponibile 
                         </label>
                     </div>
 
-                    <button type="submit" class="btn btn-dark text-white">Send Edit</button>
+                    <button type="submit" class="btn btn-dark text-white">Invia</button>
 
                 </form> 
             </div>
--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -6,10 +6,10 @@
             <div class="card col-4 py-2 bg-dark text-white">
                 <h1 class="card-title fw-bold text-center mb-3">{{$dish->name}}</h1>
                 <div class="card-body">
-                    <p class="card-text">Description: {{$dish->description}}</p>
-                    <p class="card-text">ingredients: {{$dish->ingredients}}</p>
-                    <p class="card-text">Price: {{$dish->price}}</p>
-                    <p class="card-text">Created: {{$dish->created_at}}</p>
+                    <p class="card-text">Descrizione: {{$dish->description}}</p>
+                    <p class="card-text">ingredienti: {{$dish->ingredients}}</p>
+                    <p class="card-text">Prezzo: {{$dish->price}}</p>
+                    <p class="card-text">Creato il: {{$dish->created_at}}</p>
                 </div>
             </div>
         </div>
