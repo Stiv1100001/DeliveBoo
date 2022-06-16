836e5dfc47273f3bc91dcb1106042b8ca4cf4889
Stefano Bichicchi <bichicchi.stefano@gmail.com>
2022-06-16 14:29:09 +0200

    fix table data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string("address")->nullable();
             $table->string("vat_number")->nullable();
             $table->string("email")->unique();
-            $table->timestamp("email_verified_at")->nullable();
+            // $table->timestamp("email_verified_at")->nullable();
             $table->string("password");
             $table->rememberToken();
             $table->timestamps();
