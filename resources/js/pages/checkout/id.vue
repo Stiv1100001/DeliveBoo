3ec4e97696d82b6d6a20b75cdbaedb98a4be66d8
Stefano Bichicchi <bichicchi.stefano@gmail.com>
2022-06-20 14:37:49 +0200

    fix

--- resources/js/pages/checkout/id.vue
+++ resources/js/pages/checkout/id.vue
@@ -1,35 +1,30 @@
 <template>
-<div class="text">
-    {{$route.params.id}}
-</div>
-
+  <div class="text">
+    {{ $route.params.id }}
+  </div>
 </template>
 
 <script>
 export default {
-name: "id",
-data:function(){
-    return{
-        product:[]
-    }
-
-},
-methods:{
-    getProduct(){
-        axios.get('/api/dishes/' + this.$route.params.id)
-        .then(response => {
-            this.product = response.data.data;
-            console.log(this.product);
-        })
-    }
-},
-created(){
-        this.getProduct(this.$route.params.id);
-        console.log(this.$route.params.id);
-    }
-}
+  name: "id",
+  data: function () {
+    return {
+      product: [],
+    };
+  },
+  methods: {
+    getProduct() {
+      axios.get("/api/dishes/" + this.$route.params.id).then((response) => {
+        this.product = response.data.data;
+        console.log(this.product);
+      });
+    },
+  },
+  created() {
+    this.getProduct(this.$route.params.id);
+    console.log(this.$route.params.id);
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
