50281a37428b0d0edd1ab71f2058287e97989369
Stefano Bichicchi <bichicchi.stefano@gmail.com>
2022-06-20 14:37:21 +0200

    fix

--- resources/js/components/Product.vue
+++ resources/js/components/Product.vue
@@ -1,33 +1,25 @@
 <template>
-        <div class="col-4 mt-3">
-
-                <div class="card m-1">
-                    <h5 class="card-title">{{product.name}}</h5>
-                    <p class="card-text">{{product.description}}</p>
-                    <p class="card-text">{{product.ingredients}}</p>
-                    <p class="card-text"> &euro;{{product.price}}</p>
-                    <router-link :to="{name:'single-product', params:{id: product.id}}">
-                        <button class="btn btn-primary">ACQUISTA</button>
-                    </router-link>
-
-                </div>
-        </div>
+  <div class="col-4 mt-3">
+    <div class="card p-3">
+      <h5 class="card-title">{{ product.name }}</h5>
+      <p class="card-text">{{ product.description }}</p>
+      <p class="card-text">{{ product.ingredients }}</p>
+      <p class="card-text">&euro;{{ product.price }}</p>
+      <router-link :to="{ name: 'single-product', params: { id: product.id } }">
+        <button class="btn btn-primary">ACQUISTA</button>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Product",
+  name: "Product",
 
-    props: ['product'],
+  props: ["product"],
 
-    methods:{
-
-    }
-}
+  methods: {},
+};
 </script>
 
-<style lang="scss" scoped>
-
-
-
-</style>
+<style lang="scss" scoped></style>
--- resources/js/pages/index.vue
+++ resources/js/pages/index.vue
@@ -1,60 +1,50 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12" v-if="loading">
-                {{ loading }}
-            </div>
-                <Product v-for="(product,index) in prodotti" :key="index" :product="product"  />
-        </div>
-
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12" v-if="loading">
+        {{ loading }}
+      </div>
+      <Product
+        v-for="(product, index) in prodotti"
+        :key="index"
+        :product="product"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-
-import Product from '../components/Product';
+import Product from "../components/Product";
 export default {
-
-    name: "ExampleComponent",
-    components: {
-        Product
-    },
-    data(){
-
-        return {
-            loading: true,
-            prodotti : []
-        }
-    },
-        methods: {
-            getProduct(){
-                this.loading = true;
-                axios.get('/api/dishes')
-                .then(response => {
-                    this.prodotti = response.data.data;
-                    this.loading = false;
-                    console.log(this.prodotti);
-                })
-                /* .finally(() => {
+  name: "ExampleComponent",
+  components: {
+    Product,
+  },
+  data() {
+    return {
+      loading: true,
+      prodotti: [],
+    };
+  },
+  methods: {
+    getProduct() {
+      this.loading = true;
+      axios.get("/api/dishes").then((response) => {
+        this.prodotti = response.data.data;
+        this.loading = false;
+        console.log(this.prodotti);
+      });
+      /* .finally(() => {
                     setTimeout(() => {
                     this.loading = false;
                     }, 5000);
                 }) */
+    },
+  },
 
-
-            }
-
-
-        },
-
-        created(){
-            this.getProduct();
-        }
-
-}
-
+  created() {
+    this.getProduct();
+  },
+};
 </script>
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
