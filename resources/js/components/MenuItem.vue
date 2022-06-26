dd1ea1378173335e409673db79dce1257238794a
Stefano Bichicchi <bichicchi.stefano@gmail.com>
2022-06-26 13:15:54 +0200

    fix merge

--- resources/js/components/MenuItem.vue
+++ resources/js/components/MenuItem.vue
@@ -7,106 +7,85 @@
       <p class="card-text">Ingredienti: {{ item.ingredients }}</p>
       <p class="card-text">prezzo: {{ item.price }} &euro;</p>
 
-      <div
-        class="q-holder w-100 d-flex justify-content-between align-items-center">
-        <button
-          class="btn btn-info w-25"
-          @click="minus()"
-          :disabled="quantity.length == 0">
-          -
-        </button>
+      <div class="q-holder w-100 d-flex justify-content-between align-items-center">
+        <button class="btn btn-info w-25" @click="minus()" :disabled="quantity.length == 0">-</button>
         <span class="">{{ quantity }}</span>
         <button class="btn btn-info w-25" @click="plus()">+</button>
       </div>
-    </div>
-
-    <div
-      class="q-holder w-100 d-flex justify-content-between align-items-center">
-      <button
-        class="btn btn-info w-25"
-        @click="minus()"
-        :disabled="quantity.length == 0">
-        -
-      </button>
-      <span class="">{{ quantity }}</span>
-      <button class="btn btn-info w-25" @click="plus()">+</button>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-export default {
-  name: "MenuItem",
-  data: () => ({
-    quantity: 0,
-  }),
+  import { mapGetters } from "vuex";
+  export default {
+    name: "MenuItem",
+    data: () => ({
+      quantity: 0,
+    }),
 
-  props: {
-    item: {
-      type: Object,
-      required: true,
+    props: {
+      item: {
+        type: Object,
+        required: true,
+      },
     },
-  },
 
-  computed: {
-    ...mapGetters(["getRestaurantOrderId"]),
-  },
+    computed: {
+      ...mapGetters(["getRestaurantOrderId"]),
+    },
 
-  created() {
-    const cart = this.$store.getters.getCart;
+    created() {
+      const cart = this.$store.getters.getCart;
 
-    const dish = cart.find((el) => el.dish.id == this.item.id);
+      const dish = cart.find((el) => el.dish.id == this.item.id);
 
-    if (dish) {
-      this.quantity = dish.quantity;
-    }
-  },
-
-  methods: {
-    plus() {
-      if (
-        this.getRestaurantOrderId &&
-        this.getRestaurantOrderId !== this.item.user_id
-      ) {
-        this.$emit("insertError");
-        return;
+      if (dish) {
+        this.quantity = dish.quantity;
       }
-
-      this.quantity++;
-      this.addToCart();
     },
 
-    minus() {
-      if (this.quantity === 0) return;
-      this.quantity--;
-      this.removeFromCart();
+    methods: {
+      plus() {
+        if (this.getRestaurantOrderId && this.getRestaurantOrderId !== this.item.user_id) {
+          this.$emit("insertError");
+          return;
+        }
+
+        this.quantity++;
+        this.addToCart();
+      },
+
+      minus() {
+        if (this.quantity === 0) return;
+        this.quantity--;
+        this.removeFromCart();
+      },
+
+      addToCart() {
+        this.quantity = parseInt(this.quantity);
+        if (this.quantity <= 0) return;
+
+        const payload = {
+          dish: this.item,
+          quantity: 1,
+        };
+
+        this.$store.commit("addProductToCart", payload);
+      },
+
+      removeFromCart() {
+        this.quantity = parseInt(this.quantity);
+
+        const payload = {
+          dish: this.item,
+          quantity: 1,
+        };
+
+        this.$store.commit("removeProductFromCart", payload);
+      },
     },
-
-    addToCart() {
-      this.quantity = parseInt(this.quantity);
-      if (this.quantity <= 0) return;
-
-      const payload = {
-        dish: this.item,
-        quantity: 1,
-      };
-
-      this.$store.commit("addProductToCart", payload);
-    },
-
-    removeFromCart() {
-      this.quantity = parseInt(this.quantity);
-
-      const payload = {
-        dish: this.item,
-        quantity: 1,
-      };
-
-      this.$store.commit("removeProductFromCart", payload);
-    },
-  },
-};
+  };
 </script>
 
 <style lang="scss" scoped></style>
