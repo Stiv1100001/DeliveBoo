3b81d62f6a264c2fff1c6b02d2cec86c94537179
patriciabrsk <patriciabresku@gmail.com>
2022-06-27 21:33:58 +0200

    fixing for the 1234432 time

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,14 @@
 <div class="w-100" id="background-home-admin">
     <div class="h-100 my-opacity d-flex justify-content-center align-items-center">
         <div class="container abril-fatface-font">
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col-12 text-center">
                     <h1 class="fw-bold welcome text-white mb-5">Benvenuto, il tuo ristorante {{
                         Auth::user()->name_restaurant }} è
                         stato registrato
                     </h1>
                 </div>
-                <div class="col-12 text-center">
+                <div class="col-9 d-flex justify-content-center">
                     <a class="text-decoration-none btn btn-outline-light text-uppercase rounded-pill menu"
                         href="{{ route('admin.dishes.index') }}">
                         Menu
