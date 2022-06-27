d9b0423b1ae56e07c13349502524167ed46c855c
patriciabrsk <patriciabresku@gmail.com>
2022-06-27 19:12:03 +0200

    header admin fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,10 @@
                             @else
                             <div class="nav-item dropdown d-flex align-items-center">
                                 <img src="{{asset('storage/' . Auth::user()->image_url) }}"
-                                alt="{{Auth::user()->name_resturant}}" class="rounded-circle border border-3 border-dark me-3 d-sm-inline d-none" id="user-img-nav">
+                                alt="{{Auth::user()->name_resturant}}" class="rounded-circle border border-dark me-3 d-sm-inline d-none" id="user-img-nav">
 
                                 <div class="dropdown">
-                                    <button class="btn btn-outline-dark border border-3 border-dark rounded fw-bold fs-5" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <button class="btn btn-outline-dark rounded-pill" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                         {{ Auth::user()->name_restaurant }}
                                         <i class="fas fa-bars p-1"></i>
                                     </button>
