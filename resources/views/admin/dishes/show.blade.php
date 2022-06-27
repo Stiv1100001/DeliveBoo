c6e1ab6837950d0dd800d2dd94774eaa25359b04
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-06-27 20:34:35 +0200

    modyfy style

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -4,7 +4,7 @@
 
 <div class="w-100" id="background-home-admin"> 
     <div class="w-100 h-100 my-opacity d-flex justify-content-center align-items-center">
-            <div class="card col-10 col-sm-10 col-md-10 col-lg-8 my-bg-gradient border-0 rounded abril-fatface-font bg-cambridge-blue">
+            <div class="card col-10 col-sm-10 col-md-10 col-lg-8 my-bg-gradient border-0 rounded  bg-cambridge-blue">
                 <div class="row g-0">
                     <div class="col-6 d-none d-lg-inline p-3 shadow">
                         <div class="h-100 w-100 d-flex justify-content-center align-items-center">
@@ -13,7 +13,7 @@
                     </div>
                     <div class="col-sm-12 col-lg-6 d-flex justify-content-center align-items-center p-4">
                         <div class="card-body bg-white rounded shadow h-75 overflow-auto">
-                            <h3 class="card-text text-center fw-bold">{{$dish->name}}</h3>
+                            <h3 class="card-text text-center fw-bold abril-fatface-font">{{$dish->name}}</h3>
                             <p class="card-text text-secondary"> <span class="fw-bold fs-5 text-dark">Descrizione:</span> {{$dish->description}}</p>
                             <p class="card-text text-secondary"> <span class="fw-bold fs-5 text-dark">ingredienti:</span> {{$dish->ingredients}}</p>
                             <p class="card-text text-secondary"> <span class="fw-bold fs-5 text-dark">Prezzo:</span>: {{$dish->price}}</p>
