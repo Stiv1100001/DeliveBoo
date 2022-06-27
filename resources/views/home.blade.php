9fe5de60b31b33492d5c999bdb9915ecd29b202a
patriciabrsk <patriciabresku@gmail.com>
2022-06-27 19:18:35 +0200

    style fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
         <div class="container abril-fatface-font">
             <div class="row">
                 <div class="col-12 text-center">
-                    <h1 class="fw-bold  welcome text-white mb-5">Benvenuto, il tuo ristorante {{
+                    <h1 class="fw-bold welcome text-white mb-5">Benvenuto, il tuo ristorante {{
                         Auth::user()->name_restaurant }} è
                         stato registrato</h1>
                 </div>
                 <div class="col-12 text-center">
-                    <a class="text-decoration-none fw-bold btn btn-outline-light border border-2 border-light rounded fs-2"
+                    <a class="text-decoration-none btn btn-outline-light text-uppercase rounded-pill menu"
                         href="{{ route('admin.dishes.index') }}">Menu</a>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,24 +59,24 @@
                                         {{ Auth::user()->name_restaurant }}
                                         <i class="fas fa-bars p-1"></i>
                                     </button>
-                                    <ul class="dropdown-menu bg-dropdown border border-2 border-dark" aria-labelledby="dropdownMenuLink">
+                                    <ul class="dropdown-menu bg-dropdown border border-dark" aria-labelledby="dropdownMenuLink">
 
                                         <li class="dropdown-item text-center p-0">
-                                            <a class="text-decoration-none text-black fw-bold"  href="{{ route('home') }}" role=“button">
+                                            <a class="text-decoration-none text-black"  href="{{ route('home') }}" role=“button">
                                                 <i class="fas fa-house me-1"></i>
                                                 Home
                                             </a>
                                         </li>
                                         <li><hr class="dropdown-divider"></li>
                                         <li class="dropdown-item text-center p-0">
-                                            <a class="text-decoration-none text-black fw-bold" href="{{ route('admin.dishes.index') }}" role=“button">
+                                            <a class="text-decoration-none text-black" href="{{ route('admin.dishes.index') }}" role=“button">
                                                 <i class="fas fa-utensils me-1"></i>
                                                 Menu
                                             </a>
                                         </li>
                                         <li><hr class="dropdown-divider"></li>
                                         <li class="dropdown-item text-center p-0">
-                                            <a class="text-decoration-none text-black fw-bold" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            <a class="text-decoration-none text-black" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                                 <i class="fas fa-right-from-bracket"></i>
                                                 {{ __('Logout') }}
                                             </a>
