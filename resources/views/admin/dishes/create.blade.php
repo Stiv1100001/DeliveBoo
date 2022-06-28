84e6c4770e210135f5cdf2dfd359eac88b4bbcb8
patriciabrsk <patriciabresku@gmail.com>
2022-06-28 08:16:22 +0200

    changed form font to the font we are actually using..

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -5,8 +5,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12 col-sm-10 col-md-8 col-lg-5">
-                <div class="form-container rounded">
-                    <h1 class="text-center fw-bold text-dark mb-5">Aggiungi un piatto</h1>
+                <div class="form-container rounded-3">
+                    <h1 class="text-center text-dark mb-5">Aggiungi un piatto</h1>
 
                     <form id="create-dish" class="form-horizontal overflow-auto my-form" action="{{ route('admin.dishes.store') }}" method="POST" enctype="multipart/form-data" id="form">
                         @csrf
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group">
-                            <span class="input-icon"><img src="/img/icons8-ingredients-64.png" alt=""></span> 
+                            <span class="input-icon"><img src="/img/icons8-ingredients-64.png" alt=""></span>
                             <div>
                                 <textarea  placeholder="Ingredienti del piatto" class="form-control" name="ingredients" id="ingredients"
                                 rows="5">{{ old('ingredients') }}</textarea>
@@ -53,7 +53,7 @@
                         </div>
 
                         <div class="form-group">
-                            <span class="input-icon"><i class="fa-solid fa-euro-sign"></i></span> 
+                            <span class="input-icon"><i class="fa-solid fa-euro-sign"></i></span>
                             <div>
                                 <input placeholder="Prezzo del piatto" type="number" min="0.0" step="0.1" name="price" id="price" class="form-control">
                                 @error('price')
@@ -66,9 +66,9 @@
 
                         <div class="form-group py-3">
                             <div>
-                                <input id="img_url" type="file" class="form-control p-1 border border-2 rounded h-auto"
+                                <input id="img_url" type="file" class="form-control p-1 border border-2 rounded-3 h-auto"
                                 @error('img_url') is-invalid @enderror" name="img_url" required autofocus>
-    
+
                                 @error('img_url')
                                 <span class=" invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
